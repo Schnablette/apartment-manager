0c424ed66bc0b2dee538220a32f9363e3df11097
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-07-13 13:16:21 -0400

    preliminary css set up

---
+++ server/client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/App.css';
+
+// components
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+const NavBar = () => {
+    // TODO
+    return (
+        <div>
+            <h2></h2>
+        </div>
+    )
+  };
+
+export default NavBar;
