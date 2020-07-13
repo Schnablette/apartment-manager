b4a7631112b78c2fc15ed6470c38457c000f8a7d
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-07-13 13:52:53 -0400

    NavBar CSS added

--- server/client/src/components/NavBar.js
+++ server/client/src/components/NavBar.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import "../styles/NavBar.css"
 
 const NavBar = () => {
     // TODO
     return (
-        <div>
-            <h2></h2>
+        <div id="navBar">
+            <h1>Aparment Management</h1>
+            <ul id="navBar-list">
+                <li id="leases"><a href='#'>Leases</a></li>
+                <li id="maintenance"><a href='#'>Maintenance</a></li>
+                <li id="complaints"><a href='#'>Complaints</a></li>
+                <li id="utilities"><a href='#'>Utilities</a></li>
+            </ul>
         </div>
     )
   };
