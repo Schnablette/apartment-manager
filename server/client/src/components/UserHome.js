5bdce76245cc394789baca87420bdc3f769e6165
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-07-15 07:54:57 -0400

    userHome complete minus responsiveness

--- server/client/src/components/UserHome.js
+++ server/client/src/components/UserHome.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom"
 import "../styles/UserHome.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import NavBarUser from './NavBarUser';
 import plant from '../assets/green-plant.png'
 
-const UserHome = () => {
-    return (
-        <div>
-            <NavBarUser />
-            <div id="userHome">
-                <h1>Get Started</h1>
-                <p>Click on any navigation point to start</p>
-                <img src={plant} />
+class UserHome extends Component {
+
+    componentDidMount() {
+        document.body.style.backgroundColor = "white";
+    }
+
+    componentWillUnmount() {
+        document.body.style.backgroundColor = "var(--main)";
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 id="logo">Namely</h1>
+                <h1 id="welcome">Welcome Hill<br/>Valley Resident</h1>
+                <p className="instructions">Select an option to begin.</p>
+                <div id="userHome">
+                    <a href="/user/maintenance">
+                        <button>Write a<br/>Maintenance Report</button>
+                    </a>
+                    <a href="/user/complaint">
+                        <button>File a <br/> Resident Complaint</button>
+                    </a>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 
