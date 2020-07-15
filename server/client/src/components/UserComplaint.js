9cf0b78192236bdc7deb85aecc424f762662ca64
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-07-15 13:27:11 -0400

    complaint page styled

--- server/client/src/components/UserComplaint.js
+++ server/client/src/components/UserComplaint.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom"
-import "../styles/UserHome.css";
+import "../styles/UserComplaint.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import NavBarUser from './NavBarUser';
@@ -12,41 +12,26 @@ class UserComplaint extends Component {
         return (
             <div id="userComplaint">
                 <h1 className="logo">Namely</h1>
+                <div id="complaintBackgroundImage"></div>
                 <div className="moduleForm" id="complaintForm">
                     <h1>Resident Complaint Form</h1>
-                    <form role="form" novalidate>
-                        <label>Email address</label>
-                        <input type="text" placeholder="Enter email" />
+                    <form role="form" id="complaintForm">
+                        <label>Your Apartment</label>
+                        <input type="text" placeholder="Apt #" className="apt" />
                         <br/>
-                        <label>Email address</label>
-                        <input type="dropdown" placeholder="Enter email" />
+                        <label>Offending Apartment</label>
+                        <input type="text" placeholder="Apt #" className="apt" />
                         <br/>
-                        <label>Email address</label>
-                        <input type="text" placeholder="Enter email" />
+                        <label>Type</label>
+                        <select id="myList">
+                            <option value="noise">noise</option>
+                            <option value="smell">smell</option>
+                            <option value="other">other</option>
+                        </select>
                         <br/>
+                        <label>Description</label>
+                        <input type="text" placeholder="Description" id="description" />
                     </form>
-                    {/* <Form>
-                        <Form.Group className="formGroup" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Your Apartment Number</Form.Label>
-                            <Form.Control size="lg" className="formElements" type="text" placeholder="Apt Number" />
-                        </Form.Group>
-                        <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Type of Complaint</Form.Label>
-                            <Form.Control size="lg" className="formElements" as="select">
-                            <option>Noise</option>
-                            <option>Smell</option>
-                            <option>Other</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="exampleForm.ControlInput1">
-                            <Form.Label>Offending Apartment Number</Form.Label>
-                            <Form.Control size="lg" className="formElements" type="text" placeholder="Apt Number" />
-                        </Form.Group>
-                        <Form.Group controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>Description of Offense</Form.Label>
-                            <Form.Control size="lg" className="formElements" as="textarea" rows="3" />
-                        </Form.Group>
-                    </Form> */}
                 </div>
             </div>
         )
