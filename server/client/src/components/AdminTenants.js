6a73ebea008a96d29cc8f11309729dd2d8401515
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-07-19 11:47:05 -0400

    remove tenant works

--- server/client/src/components/AdminTenants.js
+++ server/client/src/components/AdminTenants.js
@@ -3,21 +3,30 @@ import "../styles/AdminTenants.css";
 import AdminNav from "./AdminNav";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { getTenants } from "../actions/index";
+import { getTenants, removeTenant } from "../actions/index";
 
 class AdminTenants extends React.Component {
     componentDidMount() {
         this.props.getTenants()
     }
 
+    moveOutTenant(event) {
+        let id = event.target.parentElement.parentElement.id
+        this.props.removeTenant(id)
+    }
+
+    componentDidUpdate() {
+        this.props.getTenants()
+    }
+
     renderTenants() {
         return this.props.tenants.map((tenant) => {
             return (
-                <tr key={tenant._id}>
+                <tr key={tenant._id} id={tenant._id}>
                     <td>{tenant.aptNumber}</td>
                     <td>{tenant.tenants}</td>
                     <td>{tenant.name}</td>
-                    <td><button>Move out</button></td>
+                    <td><button onClick={this.moveOutTenant.bind(this)}>Move out</button></td>
                 </tr>
             )
         })
@@ -53,7 +62,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ getTenants }, dispatch);
+    return bindActionCreators({ getTenants, removeTenant }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AdminTenants);
